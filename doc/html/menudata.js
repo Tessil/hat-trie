22c3a1d5de7a98a70fa22c0e96933b5af4872ee4
Tessil <tessil@gmx.com>
2018-08-12 12:48:52 +0200

    Update documentation.

--- doc/html/menudata.js
+++ doc/html/menudata.js
@@ -41,4 +41,7 @@ var menudata={children:[
 {text:"Typedefs",url:"functions_type.html"},
 {text:"Related Functions",url:"functions_rela.html"}]}]},
 {text:"Files",url:"files.html",children:[
-{text:"File List",url:"files.html"}]}]}
+{text:"File List",url:"files.html"},
+{text:"File Members",url:"globals.html",children:[
+{text:"All",url:"globals.html"},
+{text:"Macros",url:"globals_defs.html"}]}]}]}
