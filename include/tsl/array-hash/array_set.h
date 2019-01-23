267f5f64f7c99cd0dc21ce4827bf615c7e978cf9
Tessil <tessil@gmx.com>
2019-01-23 20:19:29 +0100

    Update array-hash.

--- include/tsl/array-hash/array_set.h
+++ include/tsl/array-hash/array_set.h
@@ -578,7 +578,7 @@ public:
      * Serialize the set through the `serializer` parameter.
      * 
      * The `serializer` parameter must be a function object that supports the following calls:
-     *  - `void operator()(const U& value);` where the types `std::uint64_t` and `float` must be supported for U.
+     *  - `template<typename U> void operator()(const U& value);` where the types `std::uint64_t` and `float` must be supported for U.
      *  - `void operator()(const CharT* value, std::size_t value_size);`
      * 
      * The implementation leaves binary compatibilty (endianness, IEEE 754 for floats, ...) of the types it serializes
@@ -593,13 +593,13 @@ public:
      * Deserialize a previouly serialized set through the `deserializer` parameter.
      * 
      * The `deserializer` parameter must be a function object that supports the following calls:
-     *  - `template<typename U> U operator()();` where the types `std::uint64_t`, `float` and `T` must be supported for U.
+     *  - `template<typename U> U operator()();` where the types `std::uint64_t` and `float` must be supported for U.
      *  - `void operator()(CharT* value_out, std::size_t value_size);`
      * 
      * If the deserialized hash set type is hash compatible with the serialized set, the deserialization process can be
      * sped up by setting `hash_compatible` to true. To be hash compatible, the Hash (take care of the 32-bits vs 64 bits), 
      * KeyEqual, GrowthPolicy, StoreNullTerminator, KeySizeT and IndexSizeT must behave the same than the ones used on the 
-     * serialized set. Otherwise the behaviour is undefined with `hash_compatible` sets to true, .
+     * serialized set. Otherwise the behaviour is undefined with `hash_compatible` sets to true.
      * 
      * The behaviour is undefined if the type `CharT` of the `array_set` is not the same as the
      * type used during serialization.
